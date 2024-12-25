f0a448243476970f0c7d3efef3ca86863ec50fee
JJOMBWE NATHAN <140011996+JJOMBWE-INNOVATIVE@users.noreply.github.com>
2024-12-26 01:25:29 +0300

    Add files via upload

---
+++ maximum pooling.py
@@ -0,0 +1,102 @@
+import numpy as np
+from itertools import product
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+def show_kernel(kernel, label=True, digits=None, text_size=28):
+    # Format kernel
+    kernel = np.array(kernel)
+    if digits is not None:
+        kernel = kernel.round(digits)
+
+    # Plot kernel
+    cmap = plt.get_cmap('Blues_r')
+    plt.imshow(kernel, cmap=cmap)
+    rows, cols = kernel.shape
+    thresh = (kernel.max()+kernel.min())/2
+    # Optionally, add value labels
+    if label:
+        for i, j in product(range(rows), range(cols)):
+            val = kernel[i, j]
+            color = cmap(0) if val > thresh else cmap(255)
+            plt.text(j, i, val, 
+                     color=color, size=text_size,
+                     horizontalalignment='center', verticalalignment='center')
+    plt.xticks([])
+    plt.yticks([])
+
+# Get  the image
+
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+plt.rc('image', cmap='magma')
+
+image_path = "C:/Users/Admin/Desktop/RESEARCH TRANSLATION/volvo_car.jpg"
+image = tf.io.read_file(image_path)
+image = tf.io.decode_jpeg(image)
+
+plt.figure(figsize=(6, 6))
+plt.imshow(tf.squeeze(image), cmap='gray')
+plt.axis('off')
+plt.show();
+
+# Define the kernel, it defines the value of pixels picked from an image
+
+kernel = tf.constant([
+    [-1, -1, -1],
+    [-1,  8, -1],
+    [-1, -1, -1],
+])
+
+plt.figure(figsize=(3, 3))
+show_kernel(kernel)
+
+# Reformat for batch compatibility.
+image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+image = tf.expand_dims(image, axis=0)
+kernel = tf.reshape(kernel, [*kernel.shape, 1, 1])
+
+
+# WE filter the image to show the features to be extracted
+
+image_filter = tf.nn.conv2d(
+    input=image,
+    filters=kernel,
+    strides=1,
+    padding='SAME',
+)
+
+plt.figure(figsize=(6, 6))
+plt.imshow(tf.squeeze(image_filter))
+plt.axis('off')
+plt.show();
+
+# WE use the ReLu activation to detect the image features to be extrated
+# also caaled feature mapping
+
+image_detect = tf.nn.relu(image_filter)
+plt.figure(figsize=(6, 6))
+plt.imshow(tf.squeeze(image_detect))
+plt.axis('off')
+plt.show();
+
+
+# Applying the image pool
+
+image_condense = tf.nn.pool(
+    input=image_detect, # image in the Detect step above
+    window_shape=(2, 2),
+    pooling_type='MAX',
+    # we'll see what these do in the next lesson!
+    strides=(2, 2),
+    padding='SAME',
+)
+
+plt.figure(figsize=(6, 6))
+plt.imshow(tf.squeeze(image_condense))
+plt.axis('off')
+plt.show();
